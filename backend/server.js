5f0b52bc3cee8ae50ef8443f41ebaa2dff46bbdd
henryprinceton <henryherrington@gmail.com>
2023-02-11 17:48:03 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,17 @@ const app = express();
 const port = 8080;
 
 app.get('/', function (req, res) {
- return res.send('Hello world');
+ return res.send('Monkey see, monkey do!');
 });
 
+app.post('/login', function (req, res) {
+    return res.send(true);
+});
+
+// POST method route
+app.post('/create-account', (req, res) => {
+    res.send('miles ingmar tuncel')
+})
+   
 console.log('Now listening on port ' + port + '...');
 app.listen(process.env.PORT || port);
